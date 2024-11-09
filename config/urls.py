dabc908b90fddc6cf3f1a18f7d630f782ab7f4ef
BigFlat98 <hdj9836@naver.com>
2024-11-09 17:17:52 +0900

    add index.html

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,11 @@ from django.urls import path, include
 from trade import urls as trade_urls
 from trade import views as trade_views
 from common import urls as common_urls
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trade/', include(trade_urls)),
     path('common/', include(common_urls)),
-    path('',trade_views.index , name='home' ),
+    path('',HomeView.as_view() , name='home' ),
 ]
